cb92d2166c2d9e05c5e13a2f438a8d22ce87ce02
slavasob@gmail.com <slavasob@gmail.com>
2023-11-22 19:29:31 +0300

    added Masha

---
+++ EX5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("enter username");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Masha")
+{
+    Console.WriteLine("This is Masha");
+
+
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+
+}
